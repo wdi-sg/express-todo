be20ebaffdc57de5f0a0741778e56ab932c10d5e
Rstinkho <t.turchinskiy@bk.ru>
2018-09-29 10:23:41 +0800

    added form to submit date, deadline and action. Save data in todo.json

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const jsonfile = require('jsonfile');
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({
+extended: true
+}));
 
 // this line below, sets a layout look to your express project
 const reactEngine = require('express-react-views').createEngine();
@@ -11,11 +17,39 @@ app.set('views', __dirname + '/views');
 // this line sets react to be the default view engine
 app.set('view engine', 'jsx');
 
-app.get('/', (req, res) => {
+app.get('/default', (req, res) => {
 
-  res.render('default')
-
+    res.render('default')
 
 })
+
+app.get('/', (req, res) => {
+    let html = "<html>";
+    html += "<body>";
+    html += '<form method="POST" action="/default1">';
+    html += "What should you do today? <br>" ;
+    html += '<input type="text" name="date"> <br>';
+    html += '<input type="text" name="whatToDo"> <br>';
+    html += '<input type="text" name="deadline"> <br>';
+    html += '<input type="submit" value="Submit">';
+    html += "</form>";
+    html += "</body>";
+    html += "</html>";
+res.send( html );
+});
+
+app.post('/default1',(request, response) =>{
+
+    let file = 'todo.json';
+
+    const obj = request.body;
+
+    jsonfile.writeFile(file, obj, function (err) {
+      if (err) console.log("ERROR:",err)
+
+    console.log(request.body)
+    response.send(request.body);
+    })
+});
 
 app.listen (3001);
